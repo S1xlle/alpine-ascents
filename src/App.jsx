b4177e0161be71a2d6466bf56d4a7fb2883b0e6c
Shaikha <shykht932@gmail.com>
2026-06-04 22:27:17 +0300

    added gear and techniques section with advanced hover effects

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import History from './components/History'; // استدعاء قسم التاريخ الجديد
+import Gear from './components/Gear';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Header />
       <Hero />
       <History /> {/* إضافة قسم التاريخ هنا */}
-      
+      <Gear />
       {/* الأقسام الباقية بنضيفها تحتها بالترتيب حبة حبة */}
     </div>
   );
