ca3b3248871fca0cd08e5504a9586483d5a87aeb
Shaikha <shykht932@gmail.com>
2026-06-08 02:40:39 +0300

    add gear images and update Pack component

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import Hero from './components/Hero';
 import History from './components/History'; 
 import Types from './components/Types';
 import Pack from './components/Pack';
+import Shelter from './components/Shelter';
 
 // استدعاء الملف الأساسي (البوصلة)
 import Techniques from "./components/Techniques";
 
 // استدعاء الملف الاحتياطي باسم مختلف تماماً عشان ما يصير أي خطأ
-import TechniquesBackup from "./components/TechniquesBackup";
+
 
 function App() {
   return (
@@ -22,9 +23,10 @@ function App() {
       
       {/* 🧭 هنا البوصلة الأساسية حقتك ثابتة وما تتغير ولا نلمسها */}
       <Techniques />
+      <Shelter />
       
       {/* 🧪 هنا ملف التجارب الاحتياطي اللي بنغير فيه على راحتنا كل شوية */}
-      <TechniquesBackup />
+      
       
       {/* الأقسام الباقية بنضيفها تحتها بالترتيب حبة حبة */}
     </div>
